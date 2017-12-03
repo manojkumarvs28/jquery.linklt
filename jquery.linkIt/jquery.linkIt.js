8c6916fa59b54edb17f6994c071e4378bac2c268
Manoj V Sarangmath <manojkumarvs28@gmail.com>
2017-12-03 18:41:56 +0530

    0,2.0 release

---
+++ jquery.linkIt/jquery.linkIt.js
@@ -0,0 +1,25 @@
+(function($){
+	$.fn.linkIt = function(options){
+		//Default Settings
+		var settings = $.extend({
+			href 	:null,
+			text	:null,
+			target	:'_self'
+		},options);
+		
+		//Validation
+		if(settings.href === null){
+			console.log('You need an href option to work');
+			return this;
+		}
+		return this.each(function(){
+			var object=$(this);
+			if(settings.text === null){
+				settings.text = object.text();
+			}
+			object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+		});
+				
+		
+	}
+}(jQuery));
